6b2895ef4fb7407327ff76a18c8a7c016ba7aa3a
加藤直人 <kato@Galadbolg.local>
2014-11-14 14:56:45 +0900

    fix capistrano config

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,4 +1,4 @@
-server '54.64.132.228',port: 22, user: 'ec2-user', roles: %w{app web db}, ssh_options: {
+server '54.65.87.57',port: 22, user: 'ec2-user', roles: %w{app web db}, ssh_options: {
     keys: %(~/.ssh/MyFirstEC2KeyPair.pem),
     auth_methods: %w(publickey)
 }
